e9c567bff1cb9a01733eaf5d41ff6135bfdf04d9
Koorochka <artemij@koorochka.ru>
2019-05-04 23:23:45 +0300

    Add all alerts variants

--- matrix/components/matrix/feedback.form/templates/.default/template.php
+++ matrix/components/matrix/feedback.form/templates/.default/template.php
@@ -19,11 +19,28 @@ Loc::loadLanguageFile(__FILE__);
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
 
-<form class="form-horizontal" role="form">
+<form class="form-horizontal"
+      role="form">
+
+    <div class="alert alert-danger alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <strong>Ошибка!</strong> Текст ошибки
+    </div>
+
+    <div class="alert alert-warning alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <strong>Предупреждение!</strong> Текст предупреждения
+    </div>
+
+    <div class="alert alert-success alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <strong>Успех!</strong> Успешный текст
+    </div>
+
 
     <div class="form-group">
-        <label for="feedback-name" class="col-sm-3 control-label"><?=Loc::getMessage("FEEDBACK_NAME")?></label>
-        <div class="col-sm-8">
+        <label for="feedback-name" class="col-sm-4 control-label"><?=Loc::getMessage("FEEDBACK_NAME")?></label>
+        <div class="col-sm-7">
             <input type="text"
                    class="form-control"
                    id="feedback-name"
@@ -33,8 +50,8 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
     </div>
 
     <div class="form-group">
-        <label for="feedback-mail" class="col-sm-3 control-label"><?=Loc::getMessage("FEEDBACK_EMAIL")?></label>
-        <div class="col-sm-9">
+        <label for="feedback-mail" class="col-sm-4 control-label"><?=Loc::getMessage("FEEDBACK_EMAIL")?></label>
+        <div class="col-sm-7">
             <input type="email"
                    class="form-control"
                    id="feedback-mail"
@@ -44,8 +61,8 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
     </div>
 
     <div class="form-group">
-        <label for="feedback-phone" class="col-sm-3 control-label"><?=Loc::getMessage("FEEDBACK_PHONE")?></label>
-        <div class="col-sm-8">
+        <label for="feedback-phone" class="col-sm-4 control-label"><?=Loc::getMessage("FEEDBACK_PHONE")?></label>
+        <div class="col-sm-7">
             <input type="tel"
                    class="form-control"
                    id="feedback-phone"
@@ -55,21 +72,22 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
     </div>
 
     <div class="form-group">
-        <label for="feedback-message" class="col-sm-3 control-label"><?=Loc::getMessage("FEEDBACK_MESSAGE")?></label>
-        <div class="col-sm-8">
+        <label for="feedback-message" class="col-sm-4 control-label"><?=Loc::getMessage("FEEDBACK_MESSAGE")?></label>
+        <div class="col-sm-7">
             <textarea class="form-control"
                       id="feedback-message"
                       name="message"
+                      placeholder="<?=Loc::getMessage("FEEDBACK_MESSAGE_PLACEHOLDER")?>"
                       rows="3"></textarea>
         </div>
     </div>
 
     <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-4 col-sm-10">
             <input type="submit"
                    class="btn btn-danger"
                    value="<?=Loc::getMessage("FEEDBACK_SUBMIT")?>">
         </div>
     </div>
-    
+
 </form>
