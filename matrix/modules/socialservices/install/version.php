8a43c44c17198c61d16acbeeeca3107af4a021f6
Koorochka <artemij@koorochka.ru>
2019-05-05 14:16:41 +0300

    Bitrix sampe

---
+++ matrix/modules/socialservices/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "18.0.2",
+	"VERSION_DATE" => "2018-09-04 15:27:14"
+);
---
+++ matrix/modules/socialservices/lang/ua/classes/general/facebook.php
@@ -0,0 +1,8 @@
+<?
+$MESS["socserv_fb_id"] = "Ідентифікатор застосунку (App ID):";
+$MESS["socserv_fb_secret"] = "Секретний код застосунку (App Secret):";
+$MESS["socserv_fb_note"] = "Використовуйте ваш обліковий запис на Facebook.com для входу на сайт.";
+$MESS["socserv_fb_sett_note"] = "Для отримання кодів необхідно <a href=\"https://developers.facebook.com/apps/\">зареєструвати застосунок</a> Facebook.";
+$MESS["socserv_fb_note_intranet"] = "Увійти, використовуючи акаунт Facebook.";
+$MESS["socserv_fb_sett_note1"] = "Для отримання кодів необхідно <a href=\"https://developers.facebook.com/apps/\">зареєструвати додаток</a> Facebook.<br><br>У додатку увімкніть опцію \"Вхід через Facebook\" <br> В поле \"Дійсні URL-адреси для перенаправлення OAuth\" вкажіть адресу <a href=\"#URL#\">#URL#</a>";
+?>
