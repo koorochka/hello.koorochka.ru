dc08c47408b8f2a7ea8bb7093dd9792af56803d7
Koorochka <artemij@koorochka.ru>
2019-05-16 12:06:05 +0300

    Form width the validation

--- matrix/components/matrix/feedback.form/class.php
+++ matrix/components/matrix/feedback.form/class.php
@@ -14,6 +14,11 @@
  * @param CMatrixComponent $this
  */
 
+use Koorochka\Persona\FeedbackTable,
+    Matrix\Main\Loader,
+    Matrix\Main\Localization\Loc,
+    Matrix\Main\Type\DateTime;
+
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 if(class_exists("MatrixFeedbackFormComponent"))
@@ -21,6 +26,8 @@ if(class_exists("MatrixFeedbackFormComponent"))
 
 class MatrixFeedbackFormComponent extends CMatrixComponent
 {
+    private $_fields;
+
     // <editor-fold defaultstate="collapsed" desc="# Component params">
     /**
      * @param $arParams
@@ -33,22 +40,131 @@ class MatrixFeedbackFormComponent extends CMatrixComponent
 	}
     // </editor-fold>
 
+    // <editor-fold defaultstate="collapsed" desc="# Fields">
+    /**
+     * @return mixed
+     */
+    public function getFields()
+    {
+        return $this->_fields;
+    }
+
+    /**
+     * @param mixed $fields
+     */
+    public function setFields()
+    {
+        $arFields = array();
+        $fields = FeedbackTable::getMap();
+
+        foreach ($this->arParams["FIELDS"] as $FIELD){
+            if(is_set($fields[$FIELD])){
+                $arFields[$FIELD] = array_merge($fields[$FIELD], array("value" => ""));
+            }
+        }
+
+        $this->_fields = $arFields;
+    }
+
+    public function clearFields(){
+        foreach ($this->_fields as $id=>$field){
+            $this->_fields[$id]["value"] = false;
+        }
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="# Respond">
+    public function responce()
+    {
+        global $request;
+        $arFields = array();
+        // Set fields
+        foreach ($this->_fields as $id=>$field){
+            $arFields[$id] = trim(htmlspecialchars($request->get("feedback-".$id)));
+            $this->_fields[$id]["value"] = $arFields[$id];
+            // validation
+            switch ($id){
+                case "EMAIL":
+                    if(!check_email($arFields[$id])){
+                        $this->arResult["MESSAGE"] = array(
+                            "TYPE" => "ERROR",
+                            "TEXT" => Loc::getMessage("FEEDBACK_TABLE_ERROR_EMAIL")
+                        );
+                    }
+                    break;
+                case "PHONE":
+                    #$phone = "+7(495) 123-25-52";
+                    if(!preg_match(
+                        "#^[-+0-9()\s]+$#",
+                        $arFields[$id],
+                        $out
+                    ))
+                    {
+                        $this->arResult["MESSAGE"] = array(
+                            "TYPE" => "ERROR",
+                            "TEXT" => Loc::getMessage("FEEDBACK_TABLE_ERROR_PHONE")
+                        );
+                    }
+                    break;
+            }
+
+        }
+        // ad to db
+        $arFields["TIMESTAMP_X"] = new DateTime();
+        $arFields["ACTIVE"] = "Y";
+        $arFields["SORT"] = 100;
+
+        if($this->arResult["MESSAGE"]["TYPE"] !== "ERROR"){
+            d("GO");
+        }
+
+        /*
+        $result = FeedbackTable::add($arFields);
+        if($result->isSuccess()){
+            // claer values in fields
+            $this->clearFields();
+            // create message
+            $this->arResult["MESSAGE"] = array(
+                "TYPE" => "SUCCESS",
+                "TEXT" => Loc::getMessage("FEEDBACK_TABLE_SUCCESS_ADD")
+            );
+            // Todo Send letter
+
+        }
+        else{
+            $this->arResult["MESSAGE"] = array(
+                "TYPE" => "ERROR",
+                "TEXT" => $result->getErrorMessages()
+            );
+        }
+        */
+
+    }
+
+    // </editor-fold>
+
     /**
      * Execution
      */
     public function executeComponent()
     {
-
+        if(!Loader::includeModule("koorochka.persona"))
+            return false;
+        global $request;
         /**
          * start work with cache
          */
         if($this->startResultCache(false))
         {
-
+            $this->setFields();
             $this->endResultCache();
         }
 
+        if($request->isPost()){
+            $this->responce();
+        }
 
+        $this->arResult["FILDS"] = $this->getFields();
 
         $this->IncludeComponentTemplate();
     }
